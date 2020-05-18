e34d0281effe076d67e82fa3c034667cdd19342a
appdevgenie <appdevgenie@gmail.com>
2020-05-18 19:55:33 +0200

    add shared prefs

--- app/src/main/java/com/appdevgenie/notekeeper/MainActivity.java
+++ app/src/main/java/com/appdevgenie/notekeeper/MainActivity.java
@@ -1,10 +1,12 @@
 package com.appdevgenie.notekeeper;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,7 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -71,6 +74,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onResume() {
         super.onResume();
         noteRecyclerAdapter.notifyDataSetChanged();
+        updateNavHeader();
+    }
+
+    private void updateNavHeader() {
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        TextView textUserName = headerView.findViewById(R.id.text_user_name);
+        TextView textEmailAddress = headerView.findViewById(R.id.text_email_address);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String userName = prefs.getString("user_display_name", "");
+        String emailAddress = prefs.getString("user_email_address", "");
+
+        textUserName.setText(userName);
+        textEmailAddress.setText(emailAddress);
     }
 
     private void initialiseDisplayContent() {
@@ -100,7 +118,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         menu.findItem(id).setChecked(true);
     }
 
-    private void displayCourses(){
+    private void displayCourses() {
         recyclerItems.setLayoutManager(gridLayoutManager);
         recyclerItems.setAdapter(courseRecyclerAdapter);
 
@@ -124,6 +142,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
@@ -137,10 +165,22 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             displayNotes();
         } else if (id == R.id.nav_courses) {
             displayCourses();
+        } else if (id == R.id.nav_share) {
+            handleShare();
+        } else if (id == R.id.nav_send) {
+
         }
 
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void handleShare() {
+        View view = findViewById(R.id.list_items);
+        Snackbar
+                .make(view, "Share to - " +
+                        PreferenceManager.getDefaultSharedPreferences(this).getString("user_favorite_social", ""),
+                        Snackbar.LENGTH_LONG).show();
     }
 
     private void handleSelection(int message_id) {
